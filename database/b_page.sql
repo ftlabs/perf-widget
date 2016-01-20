226278f9a73533ab19af93f07638626544d4c47a
Jake Champion <jakechampion.jake2@googlemail.com>
2016-01-20 12:35:49 +0000

    add type as a foreign key in page table

--- database/b_page.sql
+++ database/b_page.sql
@@ -2,5 +2,8 @@ CREATE TABLE IF NOT EXISTS `page` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(2000) NOT NULL DEFAULT '',
   `title` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  `type` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `pagetype_page` (`type`),
+  CONSTRAINT `pagetype_page` FOREIGN KEY (`type`) REFERENCES `pagetype` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
