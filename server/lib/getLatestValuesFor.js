38711551fbc1d81abcdf9a063b4bb94d68353cf0
Jake Champion <jakechampion.jake2@googlemail.com>
2016-01-27 13:50:33 +0000

    add concern property

--- server/lib/getLatestValuesFor.js
+++ server/lib/getLatestValuesFor.js
@@ -1,9 +1,10 @@
 const query = require('./database').query;
 const escape = require('mysql').escape;
 const debug = require('debug')('perf-widget:lib:getLatestValuesFor');
+const isConcerningValue = require('./isConcerningValue');
 
 module.exports = function getLatestValuesFor(url) {
-	const command = `SELECT properties.category, properties.provider, current_values.link, properties.name, properties.description, current_values.value FROM current_values JOIN properties JOIN page ON page.id = current_values.page_id AND page.url = ${escape(url)}`;
+	const command = `SELECT properties.name, properties.category, properties.provider, current_values.link, properties.name, properties.description, current_values.value FROM current_values JOIN properties JOIN page ON page.id = current_values.page_id AND page.url = ${escape(url)}`;
 
 	debug(command);
 
@@ -12,15 +13,27 @@ module.exports = function getLatestValuesFor(url) {
 	return queryResult.then(function(rows) {
 
 		const result = rows.map(function (row) {
-			return {
-				category: row.category,
-				provider: row.provider,
-				text: row.description,
-				link: row.link,
-				value: row.value
-			}
+			return isConcerningValue(row.name, row.value).then(function (concern) {
+				debug({
+					category: row.category,
+					provider: row.provider,
+					text: row.description,
+					link: row.link,
+					value: row.value,
+					concern: concern
+				});
+
+				return {
+					category: row.category,
+					provider: row.provider,
+					text: row.description,
+					link: row.link,
+					value: row.value,
+					concern: concern
+				}
+			});
 		});
 
-		return result;
+		return Promise.all(result);
 	});
 };
