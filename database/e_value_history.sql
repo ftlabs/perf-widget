86eb89fd812a63e3701315838fa150810ac4f485
Jake Champion <jakechampion.jake2@googlemail.com>
2016-01-27 11:59:18 +0000

    add extra value column, link

--- database/e_value_history.sql
+++ database/e_value_history.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `value_history` (
   `page_id` int(11) unsigned NOT NULL,
   `date` int(20) NOT NULL,
   `value` int(100) NOT NULL,
+  `link` varchar(1000),
   KEY `property_value_history` (`property_id`),
   KEY `page_value_history` (`page_id`),
   CONSTRAINT `page_value_history` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
