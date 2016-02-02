81294f37bb6dde1afb0971f1807d0f7400b307d5
Jake Champion <jakechampion.jake2@googlemail.com>
2016-02-02 20:09:17 +0000

    Use a new table to gather current values for each domain/host/product

---
+++ database/f_current_values_for_domain.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `current_values_for_domain` (
+  `property_id` int(11) unsigned NOT NULL,
+  `domain` varchar(1000) NOT NULL,
+  `value` int(100) NOT NULL,
+  KEY `property_current_values_for_domain` (`property_id`),
+  CONSTRAINT `property_current_values_for_domain` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
