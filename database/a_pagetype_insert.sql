602b7cc32ff5162a0ba915e530cc532bbce99d07
Sean Tracey <seanmtracey@gmail.com>
2016-02-04 14:43:49 +0000

    topicsId

--- database/a_pagetype_insert.sql
+++ database/a_pagetype_insert.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO `pagetype` (`type`, `pattern`)
 VALUES
-	('article','(^https?:\\/\\/next.ft.com\\/content\\/.*)|(^https?:\/\/app.ft.com\/cms\/s\/[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}.html)'),
+	('article','(^https?:\\/\\/next.ft.com\\/content\\/.*)|(^https?:\\/\\/app.ft.com\\/cms\\/s\\/[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}.html)'),
 	('homepage','(^https?:\\/\\/next.ft.com\\/(international|uk))|(^https?:\\/\\/app.ft.com\\/index_page\\/home)'),
-	('stream','(^https?:\\/\\/next.ft.com\\/stream\\/.*)|(^https?:\/\/app.ft.com\/stream\/organisationsId\/.*'),
+	('stream','(^https?:\\/\\/next.ft.com\\/stream\\/.*)|(^https?:\\/\\/app.ft.com\\/stream\\/(organisationsId|topicsId)\\/.*'),
 	('video','(^https?:\\/\\/video.ft.com\\/?.*)');
