915efbb008b5eee99fa69cbd89fc21038cb93bc6
Sean Tracey <seanmtracey@gmail.com>
2016-02-04 11:58:10 +0000

    Handling of non-SSL domains

--- database/c_properties_insert.sql
+++ database/c_properties_insert.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO `properties` (`id`, `name`, `concerning_text`, `reassuring_text`, `minimum`, `maximum`, `category`, `provider`, `better_than_ft`, `worse_than_ft`, `better_than_competitor`, `worse_than_competitor`, `better`)
 VALUES
 	(1, 'PageSpeedInsightsScore', 'This page is slow', 'This page is fast', 60, 100, 'Performance', 'Google Pagespeed', 'Fast for the FT', 'Slow for the FT', 'Faster than', 'Slower than', 'increasing'),
-	(2, 'SSLLabsGrade', 'This server is vulnverable to attack', 'This server is secure', null, 66, 'Security', 'SSL Labs', null, null, null, null, null);
+	(2, 'SSLLabsGrade', 'This server is vulnverable to attack', 'This server is secure', null, 2, 'Security', 'SSL Labs', null, null, null, null, null);
--- database/e_value_history.sql
+++ database/e_value_history.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `value_history` (
   `property_id` int(11) unsigned NOT NULL,
   `page_id` int(11) unsigned NOT NULL,
   `date` int(20) NOT NULL,
-  `value` int(100) NOT NULL,
+  `value` int(100),
   `link` varchar(1000),
   KEY `property_value_history` (`property_id`),
   KEY `page_value_history` (`page_id`),
