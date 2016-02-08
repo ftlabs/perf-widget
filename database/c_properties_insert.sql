aa4281604bf8c9f86e5c284143188dfdb6159462
Jake Champion <jakechampion.jake2@googlemail.com>
2016-02-08 17:43:28 +0000

    Add more insights from psi

--- database/c_properties_insert.sql
+++ database/c_properties_insert.sql
@@ -1,4 +1,10 @@
-INSERT IGNORE INTO `properties` (`id`, `name`, `concerning_text`, `reassuring_text`, `minimum`, `maximum`, `category`, `provider`, `better_than_ft`, `worse_than_ft`, `better_than_competitor`, `worse_than_competitor`, `better`)
+INSERT IGNORE INTO `properties` (`name`, `concerning_text`, `reassuring_text`, `minimum`, `maximum`, `category`, `provider`, `better_than_ft`, `worse_than_ft`, `better_than_competitor`, `worse_than_competitor`, `better`)
 VALUES
-	(1, 'PageSpeedInsightsScore', 'This page is slow', 'This page is fast', 60, 100, 'Performance', 'Google Pagespeed', 'Fast for the FT', 'Slow for the FT', 'Faster than', 'Slower than', 'increasing'),
-	(2, 'SSLLabsGrade', 'This server is vulnverable to attack', 'This server is secure', null, 2, 'Security', 'SSL Labs', null, null, null, null, null);
+	('PageSpeedInsightsScore', 'This page is slow', 'This page is fast', 60, 100, 'Performance', 'Google Pagespeed', 'Fast for the FT', 'Slow for the FT', 'Faster than', 'Slower than', 'increasing'),
+	('NumberOfHosts', 'This page uses too many third-parties', 'This page uses no third-parties', 0, 0, 'Performance', 'Google Pagespeed', 'Fewer third-party requests than FT other products', 'More third-party requests than other FT products', 'Fewer than', 'More than', 'decreasing'),
+	('NumberOfResources', 'This page makes too many requests', 'This page makes few requests', null, 10, 'Performance', 'Google Pagespeed', 'Fewer requests than the FT', 'More requests than the FT', 'Fewer than', 'More than', 'decreasing'),
+	('WeightOfResources', 'This page is heavy', 'This page is light', null, 10000, 'Performance', 'Google Pagespeed', 'Light for the FT', 'Heavy for the FT', 'Lighter than', 'Heavier than', 'decreasing'),
+	('SSLLabsGrade', 'This server is vulnverable to attack', 'This server is secure', null, 2, 'Security', 'SSL Labs', null, null, null, null, null);
+
+	
+
