35d205ceec416312be426a19b5808f745dcf4ea2
Jake Champion <jakechampion.jake2@googlemail.com>
2016-02-08 17:53:44 +0000

    Edit wording

--- database/c_properties_insert.sql
+++ database/c_properties_insert.sql
@@ -1,8 +1,8 @@
 INSERT IGNORE INTO `properties` (`name`, `concerning_text`, `reassuring_text`, `minimum`, `maximum`, `category`, `provider`, `better_than_ft`, `worse_than_ft`, `better_than_competitor`, `worse_than_competitor`, `better`)
 VALUES
 	('PageSpeedInsightsScore', 'This page is slow', 'This page is fast', 60, 100, 'Performance', 'Google Pagespeed', 'Fast for the FT', 'Slow for the FT', 'Faster than', 'Slower than', 'increasing'),
-	('NumberOfHosts', 'This page uses too many third-parties', 'This page uses no third-parties', 0, 0, 'Performance', 'Google Pagespeed', 'Fewer third-party requests than FT other products', 'More third-party requests than other FT products', 'Fewer than', 'More than', 'decreasing'),
-	('NumberOfResources', 'This page makes too many requests', 'This page makes few requests', null, 10, 'Performance', 'Google Pagespeed', 'Fewer requests than the FT', 'More requests than the FT', 'Fewer than', 'More than', 'decreasing'),
+	('NumberOfHosts', 'This page uses too many third-parties', 'This page uses no third-parties', 0, 0, 'Performance', 'Google Pagespeed', 'Fewer third-party requests than other FT products', 'More third-party requests than other FT products', 'Fewer third-party requests than', 'More third-party requests than', 'decreasing'),
+	('NumberOfResources', 'This page makes too many requests', 'This page makes few requests', null, 10, 'Performance', 'Google Pagespeed', 'Fewer requests than the FT', 'More requests than the FT', 'Fewer requests than', 'More requests than', 'decreasing'),
 	('WeightOfResources', 'This page is heavy', 'This page is light', null, 10000, 'Performance', 'Google Pagespeed', 'Light for the FT', 'Heavy for the FT', 'Lighter than', 'Heavier than', 'decreasing'),
 	('SSLLabsGrade', 'This server is vulnverable to attack', 'This server is secure', null, 2, 'Security', 'SSL Labs', null, null, null, null, null);
 
