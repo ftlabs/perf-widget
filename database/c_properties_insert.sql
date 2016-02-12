76df91518b7fb2a2a2df115401b781fa95134978
Jake Champion <me@jakechampion.name>
2016-02-12 11:33:58 +0000

    bump maximum resource count to 20

--- database/c_properties_insert.sql
+++ database/c_properties_insert.sql
@@ -2,7 +2,7 @@ INSERT IGNORE INTO `properties` (`name`, `concerning_text`, `reassuring_text`, `
 VALUES
 	('PageSpeedInsightsScore', 'This page is slow', 'This page is fast', 60, 100, 'Performance', 'Google Pagespeed', 'Fast for the FT', 'Slow for the FT', 'Faster than', 'Slower than', 'increasing'),
 	('NumberOfHosts', 'This page uses too many third-parties', 'This page uses no third-parties', 0, 0, 'Performance', 'Google Pagespeed', 'Fewer third-party requests than other FT products', 'More third-party requests than other FT products', 'Fewer third-party requests than', 'More third-party requests than', 'decreasing'),
-	('NumberOfResources', 'This page makes too many requests', 'This page makes few requests', null, 10, 'Performance', 'Google Pagespeed', 'Fewer requests than the FT', 'More requests than the FT', 'Fewer requests than', 'More requests than', 'decreasing'),
+	('NumberOfResources', 'This page makes too many requests', 'This page makes few requests', null, 20, 'Performance', 'Google Pagespeed', 'Fewer requests than the FT', 'More requests than the FT', 'Fewer requests than', 'More requests than', 'decreasing'),
 	('WeightOfResources', 'This page is big', 'This page is small', null, 10000, 'Performance', 'Google Pagespeed', 'Small for the FT', 'Big for the FT', 'Smaller than', 'Bigger than', 'decreasing'),
 	('SSLLabsGrade', 'This connection is vulnverable to attack', 'This connection is secure', null, 2, 'Security', 'SSL Labs', null, null, null, null, null);
 
