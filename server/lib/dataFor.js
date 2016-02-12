6ba6dd242f7e9e7affaa66edb53efea4a6053d3b
Sean Tracey <seanmtracey@gmail.com>
2016-02-12 15:58:22 +0000

    Pinging

--- server/lib/dataFor.js
+++ server/lib/dataFor.js
@@ -8,13 +8,62 @@ const domainDataFor = require('./domainDataFor');
 const fetch = require('node-fetch');
 const lru = require('lru-cache');
 
-const lruOptions = { 
+const pingCache = lru({ 
+		max: 500, 
+		maxAge: 1000 * 60 * 60 * 2 
+	})
+const insightsCache = lru({ 
 		max: 500, 
 		maxAge: 1000 * 60 * 60 * 24 
+	});
+
+const isUp = function (url){
+
+	return fetch( url, {cache: 'no-cache', timeout : 2500} )
+		.then(res => {
+			const up = res.status < 500;
+			pingCache.set(url, up);
+			debug('Is site up?', url, up);
+
+			return up;
+		})
+	;
+}
+
+const getInsights = function(url, freshInsights){
+
+	if (!freshInsights) {
+		debug('insightsCache.has(url)', insightsCache.has(url), url);
+		if (insightsCache.has(url)) {
+			const insightsPromise = bluebird.resolve(insightsCache.get(url));
+			if (insightsPromise.isFulfilled()) {
+				debug('insightsPromise.value()', insightsPromise.value())
+				return insightsPromise.value();
+			} else if (insightsPromise.isRejected()) {
+				debug(`Promise was rejected, deleting ${url} from insightsCache.`);
+				return { error : 'Unable to check this url' }
+			} else {
+				return {
+					reason: 'Gathering results'
+				};
+			}
+		}
 	}
 
-const pingCache = lru(lruOptions)
-const insightsCache = lru(lruOptions);
+	const host = parseUrl(url).host;
+	const insights = bluebird.all([pageDataFor(url, freshInsights), domainDataFor(host, freshInsights)]).then(flattenDeep);
+
+	insightsCache.set(url, insights);
+
+	insights.catch(function (err) {
+		debug(`Promise was rejected, deleting ${url} from insightsCache. ${err} `);
+	});
+
+	return {
+		reason: 'Gathering results'
+	};
+
+}
 
 module.exports = function (url, freshInsights) {
 	return Promise.resolve().then(function () {
@@ -31,70 +80,47 @@ module.exports = function (url, freshInsights) {
 			};
 		}
 
-		if (!freshInsights){
-			debug('pingCache.has(url)', pingCache.has(url), url);
+		if(!freshInsights){
+			debug('URL in pingCache', pingCache.has(url), url);
 			if (!pingCache.has(url)){
 				pingCache.set(url, null);
-				fetch(url, {cache : 'no-cache', timeout : 2500})
-					.then(res => {
-						debug(res, res.status, typeof(res.status));
-						if(res.status !== 200){
-							pingCache.set(url, false);
+				return isUp(url)
+					.then(up => {
+						if(up){
+
+							return getInsights(url, freshInsights);
+
 						} else {
-							pingCache.set(url, true);
+							return {
+								error: 'Unable to access this URL to perform insights'
+							}
 						}
-					}).
-					catch(err => {
-						debug(`An error occurred whilst checking a URL's network visility`, err);
+					})
+					.catch(() => {
+						pingCache.delete(url);
+						return {
+							error : `An error occurred when we tried to check this URL.`
+						}
 					})
 				;
-				return {
-					reason: 'Checking page can be accessed'
-				}
 			
 			} else {
 				const isAccessible = pingCache.get(url);
+				debug('isAccessible', isAccessible);
 
 				if(!isAccessible){
 					return {
 						error: 'Unable to access this URL to perform insights'
 					}
-
 				}
 
-			} 
+				return getInsights(url, freshInsights);
 
-		}
-
-		if (!freshInsights) {
-			debug('insightsCache.has(url)', insightsCache.has(url), url);
-			if (insightsCache.has(url)) {
-				const insightsPromise = bluebird.resolve(insightsCache.get(url));
-				if (insightsPromise.isFulfilled()) {
-					debug('insightsPromise.value()', insightsPromise.value())
-					return insightsPromise.value();
-				} else if (insightsPromise.isRejected()) {
-					debug(`Promise was rejected, deleting ${url} from insightsCache.`);
-					return { error : 'Unable to check this url' }
-				} else {
-					return {
-						reason: 'Gathering results'
-					};
-				}
 			}
+
 		}
 
-		const host = parseUrl(url).host;
-		const insights = bluebird.all([pageDataFor(url, freshInsights), domainDataFor(host, freshInsights)]).then(flattenDeep);
+		return getInsights(url, freshInsights);
 
-		insightsCache.set(url, insights);
-
-		insights.catch(function (err) {
-			debug(`Promise was rejected, deleting ${url} from insightsCache. ${err} `);
-		});
-
-		return {
-			reason: 'Gathering results'
-		};
 	});
 };
