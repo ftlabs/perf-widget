ec351033d76921b34f96c53d02bc7b90583b928c
Ada Rose <ada.edwards@ft.com>
2016-02-11 17:41:22 -0800

    Use bertha to get a list of blacklistted sites.

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -12,6 +12,44 @@ if (localStorage.getItem('enabled') === null) {
 	enabled = localStorage.getItem('enabled') === 'true';
 }
 
+let blackListPromise;
+(function updateBlacklist () {
+	blackListPromise = fetch(
+		'http://bertha.ig.ft.com/view/publish/gss/1_f-cR8VbcujCsjxTLyKVRcG9yp_qUbT0-FrdmwBp0OM/blacklist'
+	).then(response => response.json());
+
+	// Once an hour check for new black listed sites.
+	setTimeout(updateBlacklist , 3600 * 1000);
+}());
+
+function checkIsNotBlackListed (url) {
+
+	function escapeRegExp (str) {
+		return str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+	}
+
+	return blackListPromise.then(function (data) {
+		for (let condition of data) {
+			let testRegex;
+			if (condition.type === 'regex') {
+				testRegex = new RegExp(condition.blacklistsite);
+			}
+			if (condition.type === 'string') {
+
+				// RegExp to match from the start of the string replacing * with .*
+				// Escaping special RegExp characters.
+				testRegex = new RegExp('^' + escapeRegExp(condition.blacklistsite).replace(/\\\*/ig, '.*'));
+			}
+			if (testRegex) {
+
+				// if it matches it is blackListed
+				return !url.match(testRegex);
+			}
+		}
+		return true;
+	})
+}
+
 function emitMessage (method, data, url){
 	chrome.tabs.query({}, function (tabs){
 		tabs.forEach(function (tab) {
@@ -29,6 +67,11 @@ function* getData (url, freshInsights) {
 	let lastStatus = 202;
 	let data = null;
 	freshInsights = freshInsights === true;
+
+	const blackListed = yield checkIsNotBlackListed(url);
+	if (blackListed) {
+		throw Error('Website black listed');
+	}
 	const apiUrl = `${apiEndpoint}/api/data-for?url=${encodeURIComponent(url)}`;
 
 	const makeAPICall = function () {
