e6e9a4375f54ec53f585edea0e57125a2f19ec22
Jake Champion <jakechampion.jake2@googlemail.com>
2016-02-16 14:43:52 +0000

    remove unused files

--- server/lib/removeFromInFlight.js
+++
@@ -1,9 +0,0 @@
-const debug = require('debug')('perf-widget:lib:removeFromInFlight'); // eslint-disable-line no-unused-vars
-const query = require('./database').query;
-const escape = require('mysql').escape;
-
-module.exports = function removeFromInFlight(url) {
-	const command = `DELETE FROM in_flight WHERE in_flight.url = (${escape(url)});`;
-
-	return query(command);
-};
