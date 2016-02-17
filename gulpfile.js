cba8721643d7537e3752a8bf932be79dfab2db40
Ada Rose <ada.edwards@ft.com>
2016-02-16 16:08:29 -0800

    obt o-tracking

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const preprocess = require('gulp-preprocess');
 const uglify = require('gulp-uglify');
 const insert = require('gulp-insert');
 const env = require('dotenv').config();
-const webpack = require('gulp-webpack');
 const argv = require('yargs').argv;
 const serverDomain = process.env.SERVER_DOMAIN || 'https://ftlabs-perf-widget-test.herokuapp.com';
 const base64 = require('gulp-base64');
@@ -27,13 +26,11 @@ function getDestination () {
 
 }
 
-gulp.task('obt', function () {
+gulp.task('client', function () {
 
 	return obt.build(gulp, {
 		js: './client/src/js/main.js',
-		// sass: './client/src/scss/main.scss',
 		buildJs: 'contrast-bundle.js',
-		// buildCss: 'bundle.css',
 		buildFolder: './client/dist/'
 	});
 
@@ -50,10 +47,11 @@ gulp.task('set-service-url', function () {
 
 gulp.task('build-extension-main', ['copy-extension-files'], function () {
 
-	gulp.src('./extension/scripts/main.js')
-	.pipe(webpack({output: {
-		filename: 'main.js',
-	}}))
+	return obt.build(gulp, {
+		js: './extension/scripts/main.js',
+		buildJs: 'main.js',
+		buildFolder: 'disabled'
+	})
 	.pipe(preprocess( { context : { serviceURL : getDestination() } } ) )
 	.pipe(base64({
 		baseDir: 'client/src/'
@@ -63,17 +61,22 @@ gulp.task('build-extension-main', ['copy-extension-files'], function () {
 
 gulp.task('build-extension-background', ['copy-extension-files'], function () {
 
-	gulp.src('./extension/scripts/background.js')
-	.pipe(webpack({output: {
-		filename: 'background.js',
-	}}))
+	return obt.build(gulp, {
+		js: './extension/scripts/background.js',
+		buildJs: 'background.js',
+		buildFolder: 'disabled'
+	})
 	.pipe(preprocess( { context : { serviceURL : getDestination() } } ) )
 	.pipe(gulp.dest('./extension-dist/scripts/'));
 });
 
 gulp.task('build-extension-popup', ['copy-extension-files'], function () {
-	gulp.src('./extension/scripts/popup.js')
-	.pipe(gulp.dest('./extension-dist/scripts/'));
+
+	return obt.build(gulp, {
+		js: './extension/scripts/popup.js.js',
+		buildJs: 'popup.js',
+		buildFolder: './extension-dist/scripts/'
+	});
 });
 
 gulp.task('build-extension-manifest', ['copy-extension-files'], function () {
