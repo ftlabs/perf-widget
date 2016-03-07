36ffe74bcbd86a5721668aabfdf022be8fbc99ce
Ada Rose Edwards <ada@ada.is>
2016-03-07 09:30:04 -0800

    Handle error when db query returns undefined

--- server/lib/pageExists.js
+++ server/lib/pageExists.js
@@ -1,13 +1,21 @@
 const bluebird = require('bluebird');
 const query = require('./database').query;
 const escape = require('mysql').escape;
+const debug = require('debug')('perf-widget:lib:pageExists'); // eslint-disable-line no-unused-vars
 
-module.exports = bluebird.coroutine(function* pageExists(url) {
+const pageExists = bluebird.coroutine(function* pageExists(url) {
 	const command = `SELECT EXISTS(SELECT 1 FROM page WHERE url=${escape(url)})`
 
 	const result = yield query(command);
+
+	// Database closed the connection early return that the page does not exist
+	if (!result) {
+		return false;
+	}
 
 	const exists = result[0][Object.keys(result[0])[0]] === 1;
 
 	return exists;
 });
+
+module.exports = url => pageExists(url).catch(e => debug(e));
